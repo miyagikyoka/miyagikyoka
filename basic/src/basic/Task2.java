c028d1b97b8dd49e9c23086b8a32e16a3a2a36e2
miyagikyoka <S17155mk@toyoeiwa.ac.jp>
2020-11-04 18:28:08 +0900

    課題提出 - Javaの基礎

---
+++ basic/src/basic/Task2.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		System.out.println("hogehoge");
+		System.out.println("hogehoge2");
+	}
+
+}
